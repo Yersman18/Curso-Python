329540d450407903899b3ec2630a0d45b31ede21
Yersman18 <yersmangarzon@gmail.com>
2025-10-28 15:49:13 -0500

    mi primer comentario

---
+++ 01-helloword.py
@@ -0,0 +1,9 @@
+# esto es un comentario
+print("Hola mundo")
+print('Hola mundo con comillas simples')
+
+"""
+Este es un comentario 
+en varias 
+lineas
+"""
---
+++ 02-datos compuestos.py
@@ -0,0 +1,18 @@
+# datos compuestos
+
+# se pueden acceder a la lista y cambiar el valor 
+listaUno =  ["Hola", "soy esteban y mido:" , 1.68]
+listaUno[1] = "soy yersman y mido:" 
+print(listaUno)
+
+# se puede acceder la ubicacion del valor del conjunto pero no cambiar su valor
+tupla = ("Hola",  "soy Esteban y mido:" , 1.68)
+#tupla[1] = "jlskdjfls"
+print(tupla[1])
+
+
+# no se puede acceder a la ubicacion del valor ni tampoco cambiarlo porque
+# esta cambiando de lugar constantemente y no repite los mismos valores 
+conjunto = { "Hola",  "soy Esteban y mido:" , 1.68}
+
+
