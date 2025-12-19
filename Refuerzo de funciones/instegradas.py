3946fb013fdceae2bbe61b8b52c777bae12d14b6
Yersman18 <yersmangarzon@gmail.com>
2025-12-19 17:29:28 -0500

    ejercicio n2

--- Refuerzo de funciones/instegradas.py
+++ Refuerzo de funciones/instegradas.py
@@ -19,17 +19,73 @@
 
 #calificacion simple
 
+# def rango(nota):
+#     if nota > 0 and nota <= 5:
+#         if nota >= 3:
+#             return("aprobado")
+#         elif nota < 3:
+#             return("reprobado")
+#     elif nota < 0 or nota > 5:
+#         return "no es un numero valido"
 
+# resultado = rango(6)
+# print(resultado)
 
-def rango(nota):
-    if nota > 0 and nota <= 5:
-        if nota >= 3:
-            return("aprobado")
-        elif nota < 3:
-            return("reprobado")
-    elif nota < 0 or nota > 5:
-        return "no es un numero valido"
+# 🟢 Ejercicio 1
 
-resultado = rango(6)
+# Crea una función que reciba un número y retorne si es par o impar.
+
+def par_impar(numeros):
+    if numeros % 2 == 0:
+        return "El numero es par"
+    else:
+        return "El numero es impar"
+    
+num = int(input("ingresa un numero: "))
+resultado = par_impar(num)
 print(resultado)
 
+
+
+# 🟢 Ejercicio 2
+
+# Crea una función que reciba dos números y retorne el mayor de ellos.
+
+# 🟢 Ejercicio 3
+
+# Crea una función que reciba un nombre y retorne un saludo personalizado.
+
+# 🟢 Ejercicio 4
+
+# Crea una función que reciba una edad y retorne si es mayor o menor de edad.
+
+# 🟢 Ejercicio 5
+
+# Crea una función que reciba una nota (0 a 5) y retorne:
+
+# “Excelente”
+
+# “Aprobado”
+
+# “Reprobado”
+# (según el valor)
+
+# 🟢 Ejercicio 6
+
+# Crea una función que reciba una lista de números y retorne la suma total.
+
+# 🟢 Ejercicio 7
+
+# Crea una función que reciba una palabra y retorne cuántas letras tiene.
+
+# 🟢 Ejercicio 8
+
+# Crea una función que reciba un número y retorne una lista con los números del 1 hasta ese número.
+
+# 🟢 Ejercicio 9
+
+# Crea una función que reciba un texto y retorne si está vacío o no.
+
+# 🟢 Ejercicio 10
+
+# Crea una función que reciba dos números y una operación (+, -, *, /) y retorne el resultado.
