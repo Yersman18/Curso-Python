e51fdf30ec73ac1bae2938848f8394f9c6286d7f
Yersman18 <yersmangarzon@gmail.com>
2025-12-22 17:21:32 -0500

    practicando funciones con ejercicios basicos

--- Refuerzo de funciones/instegradas.py
+++ Refuerzo de funciones/instegradas.py
@@ -31,21 +31,84 @@
 # resultado = rango(6)
 # print(resultado)
 
+
+#👌 ejercicios sencillos 1
+
+# crea una funcion que reciba un numero y retorne el doble
+
+
+# def doble(numero):
+#     dobles = numero * 2
+#     return dobles
+
+# numero = int(input("Escribe un numero: "))
+
+# resultado = doble(numero)
+
+# print(resultado)
+
+
+# Crea una funcion que reciba un nombre y retorne un saludo
+
+
+# def saludo (nombre):
+#     return(f"Hola {nombre} como estas espero que tengas un excelente dia")
+
+# nombre = input("Dime tu nombre para poder saludarte: ")
+
+# resultado = saludo(nombre)
+
+# print(resultado)
+
+
+
+# crea una funcion que reciba una edad y retorne mayor de edad o menor de edad:
+
+# def edad(años):
+#     if años  >= 18:
+#         return "eres mayor de edad"
+#     else:
+#         return "eres menor de edad"
+
+
+
+# años = int(input("escribe tu edad: "))
+# resultado = edad(años)
+# print(resultado)
+
+
+# crea una funcion que reciba dos numeros y retorne el menor
+
+# def numero_menor(num1, num2):
+#     if num1 > num2:
+#         return "el primer numero es mayor que el segundo"
+#     elif num1 == num2:
+#         return "los dos numeros son iguales"
+#     else:
+#         return "El segundo numero es mayor que el primero"
+
+# num1 = int(input("Escribe un numero: "))
+# num2 = int(input("Escribe otro numero: "))
+
+# resultado = numero_menor(num1, num2)
+
+# print(resultado)
+
+# crea una funcion que reciba una nota de (0-5) y retorne:
+# "Excelente" (>=4)
+# "Aprobado" (>=3)
+# "Reprobado" (<3)
+
+
+
+
+
+
+
+
 # 🟢 Ejercicio 1
 
 # Crea una función que reciba un número y retorne si es par o impar.
-
-def par_impar(numeros):
-    if numeros % 2 == 0:
-        return "El numero es par"
-    else:
-        return "El numero es impar"
-    
-num = int(input("ingresa un numero: "))
-resultado = par_impar(num)
-print(resultado)
-
-
 
 # 🟢 Ejercicio 2
 
@@ -89,3 +152,7 @@ print(resultado)
 # 🟢 Ejercicio 10
 
 # Crea una función que reciba dos números y una operación (+, -, *, /) y retorne el resultado.
+
+
+
+
